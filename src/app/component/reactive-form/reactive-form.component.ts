ec71c29ea30e0d08cb5c0c8b932bbc530525d042
Avinash Balaso Jagtap <avinashjgtp10@gmail.com>
2020-02-19 09:21:42 +0530

    Wed

--- src/app/component/reactive-form/reactive-form.component.ts
+++ src/app/component/reactive-form/reactive-form.component.ts
@@ -8,6 +8,9 @@ import { FormControl,FormGroup,FormBuilder,Validators,FormArray } from '@angular
 })
 export class ReactiveFormComponent implements OnInit {
 
+  arr:FormArray;
+  formGroup:FormGroup;
+
 
   // formGroup=new FormGroup({
   //   firstName:new FormControl(),
@@ -21,25 +24,42 @@ export class ReactiveFormComponent implements OnInit {
   // });
 
 
-  formGroup = this.fb.group({
-    firstName: ['',Validators.required],
-    lastName: [''],
-    address: this.fb.group({
-      street: [''],
-      city: [''],
-      state: [''],
-      zip: ['']
-    }),
-    aliases:this.fb.array([
-      this.fb.control('')
-    ])
-  });
-
 
   constructor(private fb:FormBuilder) { }
 
   ngOnInit() {
+    this.formGroup = this.fb.group({
+      
+      address: this.fb.group({
+        street:  [''],
+        city: [''],
+        state: [''],
+        zip: ['']
+      }),
+      arr:this.fb.array([
+        this.createItem()
+      ])
+    });
   }
+
+  createItem(){
+    return this.fb.group({
+      firstName: ['',Validators.required],
+      lastName: [''],
+    })
+  }
+
+  addItem(){
+    this.arr=this.formGroup.get('arr') as FormArray
+    this.arr.push(this.createItem())
+  }
+
+  remove(index){
+    this.arr=this.formGroup.get('arr') as FormArray
+    this.formGroup.removeControl(index) 
+    console.log(index,)
+  }
+
 
 
   onSubmit(){
